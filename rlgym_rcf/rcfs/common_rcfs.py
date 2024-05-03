08f9ca7cc80df93632b5702ee7455a1a9ee9069b
Mathieu Suchet <mathieu.suchet@ecole-hexagone.com>
2024-05-03 18:59:40 +0200

    Gym and Sim setters, DistToBallRCF

--- rlgym_rcf/rcfs/common_rcfs.py
+++ rlgym_rcf/rcfs/common_rcfs.py
@@ -1,3 +1,5 @@
+import numpy as np
+from rlgym_sim.utils.common_values import SUPERSONIC_THRESHOLD
 from rlgym_sim.utils.gamestates import GameState
 
 from rlgym_rcf.rcfs.rcf_abstract import AbstractRCF
@@ -17,3 +19,20 @@ class OnCeilingRCF(AbstractRCF):
 class TouchingBallRCF(AbstractRCF):
     def replay_matching_condition(self, state: GameState):
         return state.players[0].ball_touched
+
+
+class SupersonicRCF(AbstractRCF):
+    def replay_matching_condition(self, state: GameState):
+        return np.linalg.norm(state.players[0].car_data.linear_velocity) >= SUPERSONIC_THRESHOLD
+
+
+class DistToBallRCF(AbstractRCF):
+    def __init__(self, distance_threshold: int = 1_000, before_threshold: bool = True):
+        self.distance_threshold = distance_threshold
+        self.before_threshold = before_threshold
+
+    def replay_matching_condition(self, state: GameState):
+        dist_to_ball = np.linalg.norm(state.ball.position - state.players[0].car_data.position)
+        if self.before_threshold:
+            return dist_to_ball <= self.distance_threshold
+        return dist_to_ball >= self.distance_threshold
--- rlgym_rcf/replay_converter/converter_to_env.py
+++ rlgym_rcf/replay_converter/converter_to_env.py
@@ -5,10 +5,13 @@ import numpy as np
 from rlgym.utils import StateSetter
 from rlgym.utils.state_setters import StateWrapper
 
+from rlgym_sim.utils import StateSetter as GymSetter
+from rlgym_sim.utils.state_setters import StateWrapper as GymWrapper
+
 from rlgym_rcf.utils.rcf_utils import _set_cars, _set_ball
 
 
-class ConverterToEnv(StateSetter):
+class ConverterToEnvGym(StateSetter):
     def __init__(self, replay_files):
         self.replay_files = replay_files
         self.states = np.array([])
@@ -38,5 +41,32 @@ class ConverterToEnv(StateSetter):
         _set_ball(state_wrapper, state)
         _set_cars(state_wrapper, state)
 
-        print(state_wrapper.cars[0].position)
-        print(state_wrapper.cars[0].rotation)
+class ConverterToEnvSym(GymSetter):
+    def __init__(self, replay_files):
+        self.replay_files = replay_files
+        self.states = np.array([])
+        self.probs = np.array([])
+
+    def _create_dummy_states(self):
+        return [pickle.dumps([0] * 93) * 2]
+
+    def load(self):
+        self.states = np.array([])
+        for replay_file in self.replay_files:
+            if self.states.size == 0:
+                self.states = np.load(replay_file)
+            else:
+                self.states = np.concatenate(self.states, np.load(replay_file))
+
+    def generate_probabilities(self):
+        return np.ones((self.states.shape[0])) / self.states.shape[0]
+
+    def reset(self, state_wrapper: GymWrapper):
+        if self.states.size == 0:
+            self.states = np.array(self._create_dummy_states())
+        self.probs = self.generate_probabilities()
+
+        state = np.array(pickle.loads(np.random.choice(a=self.states, p=self.probs, size=1)[0]))
+
+        _set_ball(state_wrapper, state)
+        _set_cars(state_wrapper, state)
